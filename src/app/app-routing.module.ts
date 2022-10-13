7df931973d7894e4d54abe0ff3d042ffc19f65bd
Djamal GANI <72838287+cprogrameur@users.noreply.github.com>
2022-10-13 18:50:28 +0100

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HomeComponent } from './home/home.component';
+import { InformationsComponent } from './informations/informations.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { RecyclageComponent } from './recyclage/recyclage.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'pricing',component:PricingComponent},
+  {path:'informations',component:InformationsComponent},
+  {path:'recyclage',component:RecyclageComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'**',component:FourOhFourComponent}
+]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/recyclage/recyclage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recyclage',
+  templateUrl: './recyclage.component.html',
+  styleUrls: ['./recyclage.component.scss']
+})
+export class RecyclageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
